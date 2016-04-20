7f5a0bd3822900137bd2c091c652431c54f552aa
Jennifer Eliuk <jen7maples@gmail.com>
2016-04-20 11:06:01 -0600

    add missing delimiter

--- src/voter_registration_http_api/service.clj
+++ src/voter_registration_http_api/service.clj
@@ -33,7 +33,7 @@
                                         (config [:timeouts :registration-methods-read]))]}
       ^:interceptors [(bifrost.i/update-in-response [:body :registration-methods]
                                                     [:body] identity)
-                      (bifrost.i/update-in-request [:query-params :language] keyword)]
+                      (bifrost.i/update-in-request [:query-params :language] keyword)]]
      ["/registrations"
       {:post [:post-registration (bifrost/interceptor
                                   channels/voter-register
