dca7b6ecec808ba6b39dee4f4c670df3e513149b
Wes Morgan <wesmorgan@icloud.com>
2016-12-09 16:03:11 -0700

    Compile & run uberjar in container

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,17 @@
-FROM quay.io/democracyworks/didor:latest
+FROM clojure:lein-2.7.1-alpine
 
 RUN mkdir -p /usr/src/voter-registration-http-api
 WORKDIR /usr/src/voter-registration-http-api
 
 COPY project.clj /usr/src/voter-registration-http-api/
 
-RUN lein deps
+ARG env=production
+
+RUN lein with-profile $env deps
 
 COPY . /usr/src/voter-registration-http-api
 
-RUN lein test
-RUN lein immutant war --name voter-registration-http-api --destination target --nrepl-port=11201 --nrepl-start --nrepl-host=0.0.0.0
+RUN lein with-profiles $env,test test
+RUN lein with-profile $env uberjar
+
+CMD ["java", "-jar", "target/voter-registration-http-api.jar"]
--- project.clj
+++ project.clj
@@ -3,36 +3,35 @@
   :url "https://github.com/democracyworks/voter-registration-http-api"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [turbovote.resource-config "0.2.0"]
-                 [com.novemberain/langohr "3.3.0"]
-                 [prismatic/schema "1.0.1"]
-                 [ch.qos.logback/logback-classic "1.1.3"]
+                 [com.novemberain/langohr "3.7.0"]
+                 [prismatic/schema "1.1.3"]
+                 [ch.qos.logback/logback-classic "1.1.8"]
 
                  ;; core.async has to come before pedestal or kehaar.wire-up will
                  ;; not compile. Something to do with the try-catch in
                  ;; kehaar.core/go-handler.
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [democracyworks/kehaar "0.7.0"]
+                 [org.clojure/core.async "0.2.395"]
+                 [democracyworks/kehaar "0.7.2"]
 
-                 [io.pedestal/pedestal.service "0.4.0"]
-                 [io.pedestal/pedestal.service-tools "0.4.0"]
+                 [io.pedestal/pedestal.service "0.4.1"]
+                 [io.pedestal/pedestal.service-tools "0.4.1"]
                  [democracyworks/pedestal-toolbox "0.7.0"]
 
                  ;; this has to go before pedestal.immutant
                  ;; until this is fixed:
                  ;; https://github.com/pedestal/pedestal/issues/33
-                 [org.immutant/web "2.1.0"]
-                 [io.pedestal/pedestal.immutant "0.4.0"]
-                 [org.immutant/core "2.1.0"]
+                 [org.immutant/web "2.1.5"]
+                 [io.pedestal/pedestal.immutant "0.4.1"]
+                 [org.immutant/core "2.1.5"]
                  [democracyworks/bifrost "0.1.5"]]
-  :plugins [[lein-immutant "2.0.0"]]
+  :plugins [[lein-immutant "2.1.0"]]
   :main ^:skip-aot voter-registration-http-api.server
-  :target-path "target/%s"
   :uberjar-name "voter-registration-http-api.jar"
   :profiles {:uberjar {:aot :all}
              :dev {:resource-paths ["dev-resources"]}
-             :test {:dependencies [[clj-http "2.0.0"]]
+             :test {:dependencies [[clj-http "3.4.1"]]
                     :resource-paths ["test-resources"]
                     :jvm-opts ["-Dlog-level=INFO"]}})
